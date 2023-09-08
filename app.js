194cdf866163ae677a5d17f5242c023379d169f9
unknown <sethsopheara@gmail.com>
2023-09-08 14:13:24 +0700

    intial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const upload = require('express-fileupload');
+const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
+
+const app = express();
+
+//register view engine
+app.set('view engine', 'ejs');
+
+//register static
+app.use(express.static('public')); //use for static file (css, js, img)
+app.use(express.urlencoded({extended: true})); //convert input from frontend to object in backend
+app.use(upload()); // for upload file
+
+//User
+app.use(userRoutes);
+
+app.listen(8000);
